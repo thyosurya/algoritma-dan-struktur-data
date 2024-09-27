388b2a2e711aad5c3fccd6052c742497e48876a4
thyosurya <128467675+thyosurya@users.noreply.github.com>
2024-09-27 14:51:50 +0700

    Update tugas_satu.cpp

--- tugas_satu.cpp
+++ tugas_satu.cpp
@@ -1,1 +1,38 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    float gajiPokok, tunjanganIstri = 0, tunjanganAnak = 0, gajiBersih;
+    int statusMenikah, jumlahAnak;
+
+    cout << "Masukkan gaji pokok: ";
+    cin >> gajiPokok;
+
+    cout << "Apakah karyawan sudah menikah? (1 = Ya, 0 = Tidak): ";
+    cin >> statusMenikah;
+
+    if (statusMenikah == 1) {
+        tunjanganIstri = 0.10 * gajiPokok;
+    }
+
+    cout << "Masukkan jumlah anak (0 jika tidak punya anak): ";
+    cin >> jumlahAnak;
+
+    if (jumlahAnak > 2) {
+        tunjanganAnak = 0.10 * gajiPokok;  
+    } else if (jumlahAnak > 0) {
+        tunjanganAnak = 0.05 * gajiPokok;  
+    } else {
+        tunjanganAnak = 0;  
+    }
+
+    gajiBersih = gajiPokok + tunjanganIstri + tunjanganAnak;
+
+
+    cout << fixed << setprecision(2); 
+    cout << "Gaji bersih karyawan adalah: Rp " << gajiBersih << endl;
+
+    return 0;
+}
 
